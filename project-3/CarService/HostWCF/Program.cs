016c589a665d65e4806dd7497096b01dcf791147
ioanabirsan <ioana.c.birsan@gmail.com>
2019-05-20 15:20:21 +0300

    Add server; Generate files for client; Add test client

---
+++ project-3/CarService/HostWCF/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using ObjectWCF;
+
+namespace HostWCF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Launch WCF server...");
+            ServiceHost host = new ServiceHost(typeof(CarService), new Uri("http://localhost:8000/CarService"));
+
+            foreach (ServiceEndpoint se in host.Description.Endpoints)
+                Console.WriteLine("A (address): {0} \nB (binding): {1} \nC(Contract): {2}\n", se.Address, se.Binding.Name, se.Contract.Name);
+
+            host.Open();
+
+            Console.WriteLine("Server in execution. Connections are expected...");
+            Console.WriteLine("Press Enter to stop the server!");
+            Console.ReadKey();
+
+            host.Close();
+        }
+    }
+}
---
+++ project-3/CarService/TestClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarServiceClient client = new CarServiceClient();
+
+            // Use the 'client' variable to call operations on the service.
+
+            // Always close the client.
+            Console.WriteLine("Client in execution...");
+            client.Close();
+        }
+    }
+}
