385ef1f54eaf513c220e66d79ea47ca1c9a49f19
ioanabirsan <ioana.c.birsan@gmail.com>
2019-05-20 14:08:29 +0300

    Add Post Comment example case study

---
+++ post-comment/PostComment/ObjectWCF/InterfaceComment.cs
@@ -0,0 +1,18 @@
+﻿using System.ServiceModel;
+using PostComment;
+
+namespace ObjectWCF
+{
+    [ServiceContract]
+    interface InterfaceComment
+    {
+        [OperationContract]
+        bool AddComment(Comment comment);
+
+        [OperationContract]
+        Comment UpdateComment(Comment newComment);
+
+        [OperationContract]
+        Comment GetCommentById(int id);
+    }
+}
---
+++ post-comment/PostComment/ObjectWCF/InterfacePost.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+using PostComment;
+
+namespace ObjectWCF
+{
+    [ServiceContract]
+    interface InterfacePost
+    {
+        [OperationContract]
+        bool AddPost(Post post);
+
+        [OperationContract]
+        Post UpdatePost(Post post);
+
+        [OperationContract]
+        int DeletePost(int id);
+
+        [OperationContract]
+        Post GetPostById(int id);
+
+        [OperationContract]
+        List<Post> GetPosts();
+    }
+}
