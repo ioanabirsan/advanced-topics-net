dc4efe1c4f25e446b0da5a8b6b9dc0fb1e0d3663
ioanabirsan <ioana.c.birsan@gmail.com>
2019-05-20 14:56:11 +0300

    Add object wcf project

---
+++ project-3/CarService/ObjectWCF/ICarService.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+using CarService;
+
+namespace ObjectWCF
+{
+    [ServiceContract]
+    interface ICarService
+    {
+        [OperationContract]
+        Client FindCustomer(string nume, string prenume, string phoneNumber);
+
+        [OperationContract]
+        void AddCustomer(Client client);
+
+        [OperationContract]
+        List<Client> GetAllCustomers();
+
+        [OperationContract]
+        void AddChassis(Sasiu sasiu);
+
+        [OperationContract]
+        Sasiu FindChassisByCode(string chassisCode);
+
+        [OperationContract]
+        void AddAuto(Auto auto);
+
+        [OperationContract]
+        void AddMaterial(Material material);
+
+        [OperationContract]
+        void AddOrder(Comanda comanda);
+
+        [OperationContract]
+        void AddOperation(Operatie operation);
+
+        [OperationContract]
+        void AddMecanic(Mecanic mecanic);
+
+        [OperationContract]
+        void AddImage(Imagine image);
+
+        [OperationContract]
+        void UpdateClient(Client client);
+
+        [OperationContract]
+        void UpdateOrder(Comanda order);
+
+        [OperationContract]
+        Comanda FindOrderById(int id);
+
+        [OperationContract]
+        Client FindCustomerById(int id);
+
+        [OperationContract]
+        void DeleteClient(int id);
+
+        [OperationContract]
+        bool ExistsClient(int id);
+
+        [OperationContract]
+        void DeleteOrder(int id);
+
+        [OperationContract]
+        Material FindMaterialById(int id);
+
+        [OperationContract]
+        Operatie FindOperationById(int id);
+
+        [OperationContract]
+        Mecanic FindMecanicById(int id);
+
+        [OperationContract]
+        Imagine FindImageById(int id);
+
+        [OperationContract]
+        void AddOrderDetails(DetaliuComanda orderDetails);
+
+        [OperationContract]
+        Sasiu FindChassisById(int id);
+
+        [OperationContract]
+        void UpdateChassis(Sasiu chassis);
+
+        [OperationContract]
+        void DeleteChassis(int id);
+
+        [OperationContract]
+        void UpdateMecanic(Mecanic mecanic);
+
+        [OperationContract]
+        void DeleteMecanic(int id);
+
+        [OperationContract]
+        void UpdateImage(Imagine image);
+
+        [OperationContract]
+        void DeleteImage(int id);
+
+        [OperationContract]
+        void UpdateMaterial(Material material);
+
+        [OperationContract]
+        void DeleteMaterial(int id);
+
+        [OperationContract]
+        void UpdateOperation(Operatie operation);
+
+        [OperationContract]
+        void DeleteOperation(int id);
+
+        [OperationContract]
+        bool ExistsCustomer(string email);
+    }
+}
