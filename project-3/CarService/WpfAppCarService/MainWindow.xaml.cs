98ec0e0e2b59abe1338faf8865416f1e56af0491
ioanabirsan <ioana.c.birsan@gmail.com>
2019-05-20 18:38:14 +0300

    Add initial functionality

---
+++ project-3/CarService/WpfAppCarService/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfAppCarService
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly CarServiceClient _client = new CarServiceClient();
+
+        private string connectionString =
+            @"Data source=localhost\SQLEXPRESS;initial catalog=AUTO;integrated security=True";
+
+        private readonly Style _dataGridCellStyle = new Style(typeof(DataGridCell))
+        {
+            Setters = {
+                new Setter(TextBlock.TextAlignmentProperty, TextAlignment.Center)
+            }
+        };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SearchCustomerButton_OnClick(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void ViewAllCustomersButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+
+                string queryString = "SELECT * FROM Clienti";
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(queryString, connectionString);
+                DataTable dataTable = new DataTable();
+                dataAdapter.Fill(dataTable);
+
+                sqlConnection.Close();
+
+                ApplyStyle(dataTable, CustomersDataGrid);
+            }
+        }
+
+        private void ViewAllOrdersButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+
+                string queryString = "SELECT * FROM Comenzi";
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(queryString, connectionString);
+                DataTable dataTable = new DataTable();
+                dataAdapter.Fill(dataTable);
+
+                sqlConnection.Close();
+
+                ApplyStyle(dataTable, OrdersDataGrid);
+            }
+        }
+
+        private void AddOrderButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ApplyStyle(DataTable dataTable, DataGrid dataGrid)
+        {
+            dataGrid.ItemsSource = dataTable.DefaultView;
+            dataGrid.CanUserAddRows = false;
+            dataGrid.Visibility = Visibility.Visible;
+            dataGrid.Columns[0].Visibility = Visibility.Hidden;
+            dataGrid.ColumnHeaderHeight = 50;
+            dataGrid.CellStyle = _dataGridCellStyle;
+        }
+    }
+}
