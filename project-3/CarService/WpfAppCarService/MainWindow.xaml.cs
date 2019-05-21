8683acd1750cb1218b031fcf471d3021a10d12d8
ioanabirsan <ioana.c.birsan@gmail.com>
2019-05-21 12:48:41 +0300

    Change look and feel; Finish search customer functionality

--- project-3/CarService/WpfAppCarService/MainWindow.xaml.cs
+++ project-3/CarService/WpfAppCarService/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using CarService;
 
 namespace WpfAppCarService
 {
@@ -40,14 +41,25 @@ namespace WpfAppCarService
         private void SearchCustomerButton_OnClick(object sender, RoutedEventArgs e)
         {
             CustomersDataGrid.Visibility = Visibility.Collapsed;
-            SearchCustomerDockPanel.Visibility = Visibility.Visible;
-            SearchCustomersDataGrid.Visibility = Visibility.Visible;
+            AddCustomerStackPanel.Visibility = Visibility.Collapsed;
+
+            SearchCustomerButtonsStackPanel.Visibility = Visibility.Visible;
+            SearchCustomerDataGrid.Visibility = Visibility.Visible;
+            AddCustomerCarButtonsStackPanel.Visibility = Visibility.Visible;
+            SearchCustomersInputGrid.Visibility = Visibility.Visible;
+            AddCustomerCarInputGrid.Visibility = Visibility.Visible;
+            DisplayChassisDataGrid.Visibility = Visibility.Visible;
         }
 
         private void ViewAllCustomersButton_OnClick(object sender, RoutedEventArgs e)
         {
-            SearchCustomerDockPanel.Visibility = Visibility.Hidden;
-            SearchCustomersDataGrid.Visibility = Visibility.Collapsed;
+            SearchCustomerButtonsStackPanel.Visibility = Visibility.Hidden;
+            SearchCustomersInputGrid.Visibility = Visibility.Collapsed;
+            DisplayChassisDataGrid.Visibility = Visibility.Collapsed;
+            SearchCustomerDataGrid.Visibility = Visibility.Hidden;
+            AddCustomerCarInputGrid.Visibility = Visibility.Hidden;
+            DisplayChassisDataGrid.Visibility = Visibility.Hidden;
+            AddCustomerStackPanel.Visibility = Visibility.Hidden;
 
             string queryString = "SELECT * FROM Clienti";
             ExecuteQuery(queryString, CustomersDataGrid);
@@ -55,7 +67,7 @@ namespace WpfAppCarService
 
         private void ViewAllOrdersButton_OnClick(object sender, RoutedEventArgs e)
         {
-            SearchCustomerDockPanel.Visibility = Visibility.Hidden;
+            SearchCustomerButtonsStackPanel.Visibility = Visibility.Hidden;
 
             string queryString = "SELECT * FROM Comenzi";
             ExecuteQuery(queryString, OrdersDataGrid);
@@ -103,7 +115,7 @@ namespace WpfAppCarService
             var isValidExpression = regex.IsMatch(textBox.Text);
 
             FindCustomerButton.IsEnabled = isValidExpression;
-            SearchDisplayInfoLabel.Content = !isValidExpression ? "The expression is not valid." : string.Empty;
+            SearchDisplayInfoTextBlock.Text = !isValidExpression ? "The expression is not valid." : string.Empty;
         }
 
         private void SearchCustomerFirstNameTextBox_OnPreviewTextInput(object sender, TextCompositionEventArgs e)
@@ -123,14 +135,7 @@ namespace WpfAppCarService
 
         private void SearchCustomerEmailCheckbox_OnClick(object sender, RoutedEventArgs e)
         {
-            if (SearchCustomerEmailCheckbox.IsChecked == true)
-            {
-                SearchCustomerEmailTextBox.IsEnabled = true;
-            }
-            else
-            {
-                SearchCustomerEmailTextBox.IsEnabled = false;
-            }
+            SearchCustomerEmailTextBox.IsEnabled = SearchCustomerEmailCheckbox.IsChecked == true;
         }
 
         private bool FieldsCompleted(string name, string firstName, string phoneNumber)
@@ -147,7 +152,7 @@ namespace WpfAppCarService
 
             if (!FieldsCompleted(name, firstName, phoneNumber))
             {
-                SearchDisplayInfoLabel.Content = @"Must complete mandatory fields";
+                SearchDisplayInfoTextBlock.Text = @"Must complete mandatory fields";
             }
             else
             {
@@ -156,24 +161,155 @@ namespace WpfAppCarService
 
                 if (client == null)
                 {
-                    SearchDisplayInfoLabel.Content =
+                    SearchDisplayInfoTextBlock.Text =
                         @"There is no customer with these specification.";
                 }
                 else
                 {
-                    SearchDisplayInfoLabel.Content = @"Customer cars are displayed bellow.";
-                    //panelAddCustomerCar.Visible = true;
+                    SearchDisplayInfoTextBlock.Text = @"Customer cars are displayed bellow.";
 
-                    //string getChassis = "SELECT CodSasiu, Denumire FROM Sasiuri";
-                   // ExecuteQuery(getChassis, dataGridViewChassisOptions);
+                    string getChassis = "SELECT * FROM Sasiuri";
+                    ExecuteQuery(getChassis, DisplayChassisDataGrid);
 
                     string getClientCars = $"SELECT * FROM Automobile WHERE ClientId = {client.Id}";
-                    ExecuteQuery(getClientCars, SearchCustomersDataGrid);
+                    ExecuteQuery(getClientCars, SearchCustomerDataGrid);
                 }
             }
         }
 
         private void FindNewCustomerButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            SearchCustomerNameTextBox.Text = "";
+            SearchCustomerFirstNameTextBox.Text = "";
+            SearchCustomerPhoneNumberTextBox.Text = "";
+            SearchCustomerEmailTextBox.Text = "";
+            SearchDisplayInfoTextBlock.Visibility = Visibility.Hidden;
+        }
+
+        private void AddCustomerCarAutoNumberTextBox_OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            ValidateAddCarField(AutoNumberPattern, AddCustomerCarAutoNumberTextBox);
+        }
+
+        private void AddCustomerCarChassisSeriesTextBox_OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            ValidateAddCarField(ChassisSeriesPattern, AddCustomerCarChassisSeriesTextBox);
+            
+            DataRowView row = (DataRowView)DisplayChassisDataGrid.SelectedItem;
+            string codSasiu = row["CodSasiu"].ToString();
+
+            string chassisSeries = AddCustomerCarChassisSeriesTextBox.Text;
+
+            bool isValidExpression = chassisSeries[6].Equals(codSasiu[0]) && chassisSeries[7].Equals(codSasiu[1]);
+            AddCustomerCarButton.IsEnabled = isValidExpression;
+            AddCarDisplayInfoTextBlock.Text = !isValidExpression ? "The expression is not valid." : string.Empty;
+        }
+
+        private void AddCustomerCarButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            string name = SearchCustomerNameTextBox.Text;
+            string firstName = SearchCustomerFirstNameTextBox.Text;
+            string phoneNumber = SearchCustomerPhoneNumberTextBox.Text;
+            var customer = _client.FindCustomer(name, firstName, phoneNumber);
+
+            DataRowView row = (DataRowView)DisplayChassisDataGrid.SelectedItem;
+            string codSasiu = row["CodSasiu"].ToString();
+            
+            Sasiu sasiu = _client.FindChassisByCode(codSasiu);
+
+            string autoNumber = AddCustomerCarAutoNumberTextBox.Text;
+            string chassisSeries = AddCustomerCarChassisSeriesTextBox.Text;
+            Auto auto = new Auto()
+            {
+                SasiuId = sasiu.Id,
+                ClientId = customer.Id,
+                NumarAuto = autoNumber,
+                SerieSasiu = chassisSeries
+            };
+
+            _client.AddAuto(auto);
+
+            AddCarDisplayInfoTextBlock.Text = @"Car added.";
+            AddCarDisplayInfoTextBlock.Visibility = Visibility.Visible;
+
+            string getClientCars = $"SELECT * FROM Automobile WHERE ClientId = {customer.Id}";
+            ExecuteQuery(getClientCars, SearchCustomerDataGrid);
+        }
+
+        private void AddNewCustomerCarButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            AddCarDisplayInfoTextBlock.Visibility = Visibility.Hidden;
+            AddCustomerCarAutoNumberTextBox.Text = "";
+            AddCustomerCarChassisSeriesTextBox.Text = "";
+        }
+
+        private void ValidateAddCarField(string pattern, TextBox textBox)
+        {
+            var regex = new Regex(pattern);
+            var isValidExpression = regex.IsMatch(textBox.Text);
+
+            AddCustomerCarButton.IsEnabled = isValidExpression;
+            AddCarDisplayInfoTextBlock.Text = !isValidExpression ? "The expression is not valid." : string.Empty;
+        }
+
+        private void AddCustomerButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            AddCustomerStackPanel.Visibility = Visibility.Visible;
+
+            SearchCustomerButtonsStackPanel.Visibility = Visibility.Hidden;
+            SearchCustomersInputGrid.Visibility = Visibility.Collapsed;
+            DisplayChassisDataGrid.Visibility = Visibility.Collapsed;
+            SearchCustomerDataGrid.Visibility = Visibility.Hidden;
+            AddCustomerCarInputGrid.Visibility = Visibility.Hidden;
+            DisplayChassisDataGrid.Visibility = Visibility.Hidden;
+        }
+
+        private void AddCustomerNameTextBox_OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddCustomerFirstNameTextBox_OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddCustomerAddressTextBox_OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddCustomerCityTextBox_OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddCustomerCountyTextBox_OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddCustomerPhoneNumberTextBox_OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddCustomerEmailCheckbox_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddCustomerEmailTextBox_OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddNewCustomerButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void NewCustomerButton_OnClick(object sender, RoutedEventArgs e)
         {
             throw new NotImplementedException();
         }
